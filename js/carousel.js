56da617ebb1db15f239f23179d4d958e3f48dfc4
Emmanuel <emma.maidana@gmail.com>
2022-06-27 20:27:12 -0300

    fix slideshow

--- js/carousel.js
+++ js/carousel.js
@@ -5,7 +5,6 @@ showSs();
 function showSs() {
     var i;
     var sShow = document.getElementsByClassName("slideShow");
-    dots = document.getElementsByClassName("dot");
     for (i = 0; i < sShow.length; i++) {
         sShow[i].style.display = "none";
     }
@@ -15,12 +14,12 @@ function showSs() {
     }
 
     sShow[sShowIndex - 1].style.display = "block";
-    dots[sShow - 1].className += " active";
     setTimeout(showSs, 4000);
-}
+} 
 
 function currentSlide(index) {
     var sShow = document.getElementsByClassName("slideShow");
+    dots = document.getElementsByClassName("dot");
     if (index > sShow.length) { index = 1 }
     else if (index < 1) { index = sShow.length }
     for (i = 0; i < sShow.length; i++) {
@@ -33,3 +32,4 @@ function currentSlide(index) {
     sShow[index - 1].style.display = "block";
     dots[index - 1].className += " active";
 }
+
